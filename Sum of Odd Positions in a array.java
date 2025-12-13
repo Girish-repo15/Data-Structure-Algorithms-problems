3da283377f6a48367ee521652a1ef89136f9a947
Girish-repo15 <girish15012002@gmail.com>
2025-12-13 10:21:57 +0530

    Create Sum of Odd Positions in a array.java

---
+++ Sum of Odd Positions in a array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+
+        // Odd positions in reversed array → indices: 1, 3, 5, ...
+        for (int i = 1; i < n; i += 2) {
+            sum += arr[n - 1 - i];
+        }
+
+        System.out.println(sum);
+    }
+}
